47f31846d508ddea8ff9e1392c505d6e99047877
jinlan <345743963@qq.com>
2016-11-28 13:49:29 +0800

    no message

--- app/src/main/java/com/zjn/retrofitandrxjavademo/http/MovieService.java
+++ app/src/main/java/com/zjn/retrofitandrxjavademo/http/MovieService.java
@@ -12,12 +12,9 @@ import rx.Observable;
  */
 
 public interface MovieService {
-//    @GET("top250")
-//    Call<MovieEntity> getTopMovie(@Query("start") int start, @Query("count") int count);
-
-
+    //@GET("top250")
+    //Call<MovieEntity> getTopMovie(@Query("start") int start, @Query("count") int count);
     @GET("top250")
     Observable<MovieEntity> getTopMovie(@Query("start") int start, @Query("count") int count);
-
 
 }
